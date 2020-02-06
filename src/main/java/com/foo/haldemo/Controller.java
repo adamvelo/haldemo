6fcba16536f5da04c10b294062bdd9c7227d5696
Greg Turnquist <gturnquist@pivotal.io>
2020-02-05 21:03:41 -0600

    Demonstrate the HAL serving feature.

--- src/main/java/com/foo/haldemo/Controller.java
+++ src/main/java/com/foo/haldemo/Controller.java
@@ -1,5 +1,7 @@
 package com.foo.haldemo;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
 	@GetMapping("/foo")
-	public ResponseEntity<Response> getFoo(){
-		return ResponseEntity.ok(new Response("foo"));
+	public ResponseEntity<Response> getFoo() {
+		return ResponseEntity.ok(new Response("foo", linkTo(methodOn(Controller.class).getFoo()).withSelfRel()));
 	}
-
 }
